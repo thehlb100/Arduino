7afddcd3b9b82709fd373c98ff34a06ead1d85f9
Bluefield <notshigetora@gmail.com>
2024-04-26 01:06:40 +0200

    feat: documentation

--- bluefield/Patron 1/led_counter.ino
+++ bluefield/Patron 1/led_counter.ino
@@ -1,3 +1,12 @@
+/**
+ * Copyright @ Bluefieldcreator
+ * 
+ * led_counter.ino
+ * 
+ * A simple led counter made with Arduino & C++
+ * 
+*/
+
 const int wait_time = 500;
 const int pines[] = {13, 12, 11, 10, 9};
 const int numPins = sizeof(pines) / sizeof(pines[0]);
@@ -8,6 +17,10 @@ void setup() {
   }
 }
 
+/**
+ * Sets all LEDs to a state.
+ * [int] - State (1 or 0)
+*/
 void setAll(int state) {
   for (int n = 0; n < numPins; n++) {
     digitalWrite(pines[n], state);
@@ -15,6 +28,9 @@ void setAll(int state) {
   delay(wait_time);
 }
 
+/**
+ * Animates a reverse loop across the pins.
+*/
 void reverseLoop() {
   for (int i = numPins - 1; i >= 0; i--) {
     digitalWrite(pines[i], 1);
@@ -24,6 +40,9 @@ void reverseLoop() {
   }
 }
 
+/**
+ * Animates a forward loop across the pins.
+*/
 void forwardLoop() {
   for (int n = 0; n < numPins; n++) {
     digitalWrite(pines[n], 1);
